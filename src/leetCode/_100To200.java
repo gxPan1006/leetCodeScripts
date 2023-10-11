223b47ced74f88ede6bb65b636ba9c37cac46546
gxPan <guoxunpan1006@gmail.com>
2023-10-11 11:59:21 +0800

    add some leetcode script

--- src/leetCode/_100To200.java
+++ src/leetCode/_100To200.java
@@ -250,6 +250,92 @@ public class _100To200 {
         return !("+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token));
     }
 
+    // 151. 反转字符串中的单词
+    // 先反转整个字符串。
+    // 然后遍历反转后的字符串，使用双指针找到每一个单词。
+    // 反转每一个单词。
+    // 最后，根据需要拼接每个单词。
+    // 删除多余的空格（双指针）
+    // 我们使用两个指针i和j来遍历字符串。
+    // 指针j用于查找非空格字符。
+    // 当j找到非空格字符时，我们将这个字符复制到i的位置，并移动i。
+    // 当j找到一个空格（而i指向一个单词的末尾）时，我们向i的位置复制一个空格，并移动i，而j继续移动直到找到下一个非空格字符。
+    // 这个过程会将所有单词移动到最前面，并用单个空格分隔它们。
+    public String reverseWords(String s) {
+        if (s == null) return null;
+
+        char[] a = s.toCharArray();
+        int n = a.length;
+
+        // Step 1: 反转整个字符串
+        reverse(a, 0, n - 1);
+        // Step 2: 反转每个单词
+        reverseWords(a, n);
+        // Step 3: 清理空格
+        return cleanSpaces(a, n);
+    }
+
+    private void reverse(char[] a, int i, int j) {
+        while (i < j) {
+            char t = a[i];
+            a[i++] = a[j];
+            a[j--] = t;
+        }
+    }
+
+    private void reverseWords(char[] a, int n) {
+        int i = 0, j = 0;
+
+        while (i < n) {
+            while (i < j || i < n && a[i] == ' ') i++; // 跳过空格
+            while (j < i || j < n && a[j] != ' ') j++; // 跳到下一个空格或者结尾
+            reverse(a, i, j - 1);                      // 反转单词
+        }
+    }
+
+    private String cleanSpaces(char[] a, int n) {
+        int i = 0, j = 0;
+
+        while (j < n) {
+            while (j < n && a[j] == ' ') j++;             // 跳过空格
+            while (j < n && a[j] != ' ') a[i++] = a[j++]; // 保留非空格字符
+            while (j < n && a[j] == ' ') j++;             // 跳过空格
+            if (j < n) a[i++] = ' ';                      // 保留一个空格
+        }
+
+        return new String(a).substring(0, i);
+    }
+
+
+    // 152. 乘积最大子数组
+    /** 考虑到乘积可能会有负负得正的情况，我们需要保留前面的最大值maxDP和最小值minDP。
+     从左到右迭代数组，对于每个位置，它的最大值和最小值只能从maxDP[i-1] * nums[i]、minDP[i-1] * nums[i]或nums[i]三者中产生。
+     因此，我们可以使用动态规划的方法来记录到目前位置的最大值和最小值。
+     最后的结果是所有maxDP中的最大值。*/
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int[] maxDP = new int[nums.length];
+        int[] minDP = new int[nums.length];
+
+        maxDP[0] = nums[0];
+        minDP[0] = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                maxDP[i] = Math.max(nums[i], maxDP[i - 1] * nums[i]);
+                minDP[i] = Math.min(nums[i], minDP[i - 1] * nums[i]);
+            } else {
+                maxDP[i] = Math.max(nums[i], minDP[i - 1] * nums[i]);
+                minDP[i] = Math.min(nums[i], maxDP[i - 1] * nums[i]);
+            }
+            result = Math.max(result, maxDP[i]);
+        }
+
+        return result;
+    }
+
 
     // 160. 相交链表 [hashList，]
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
